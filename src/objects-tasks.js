b86a6513ea13b147960188a0e0e8cd88bac28b73
Ria River <riria.river@gmail.com>
2024-03-02 06:21:08 +0300

    feat: implement sellTickets function

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -158,8 +158,17 @@ function makeWord(lettersObject) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  const ticketPrice = 25;
+  let change = 0;
+
+  for (let i = 0; i < queue.length; i += 1) {
+    const bill = queue[i];
+    if (bill - ticketPrice > change) return false;
+    change += 25;
+  }
+
+  return true;
 }
 
 /**
